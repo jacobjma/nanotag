a47a554091fc47ea44c19d19af6be7415e837cd0
jacob <jacob.jma@gmail.com>
2021-07-13 12:13:48 +0200

    WIP

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nanotag',
+    version='0.1',
+    description='nanotag',
+    author='Jacob Madsen',
+    author_email='jacob.madsen@univie.ac.at',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
